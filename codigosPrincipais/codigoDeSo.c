c8c5aa8fb790d35371616e6d7d111fae4a4c2ab9
joao <mjoao8219@gmail.com>
2026-06-02 16:19:16 -0300

    coisas

--- codigosPrincipais/codigoDeSo.c
+++ codigosPrincipais/codigoDeSo.c
@@ -5,6 +5,7 @@
 #include<sys/wait.h>
 
 #define MAX_LINE 80
+#define MAX_ARGS (MAX_LINE / 2 + 1)
 #define HISTORY_SIZE 10
 
 char history[HISTORY_SIZE][MAX_LINE];
@@ -30,26 +31,96 @@ void show_history(){
     }
 }
 
-int parse_command(char input[], char *args[]){
-    int i = 0, background = 0;
-    char *token = strtok(input, " ");
+int get_last_command(char command[]) {
+    int index;
 
-    while(token != NULL){
-        args[i] = token;
-        i++;
-        token = strtok(NULL," ");
+    if (history_count == 0) {
+        return 0;
     }
+    index = (history_count - 1) % HISTORY_SIZE;
+    strcpy(command, history[index]);
+    return 1;
+}
+
+ 
+
+int get_command_by_number(int number, char command[]) {
+    int start;
+    int index;
+
+    if (history_count < HISTORY_SIZE) {
+        start = 1;
+    } else {
+        start = history_count - HISTORY_SIZE + 1;
+    }
+    if (number < start || number > history_count) {
+        return 0;
+    }
+    index = (number - 1) % HISTORY_SIZE;
+
+    strcpy(command, history[index]);
+    return 1;
+}
+
+int parse_command(char input[], char *args[]){
+    int i = 0;
+
+    int background = 0;
+
+    char *token = strtok(input, " \t");
+
+ 
+
+    while (token != NULL && i < MAX_ARGS - 1) {
+
+        args[i] = token;
+
+        i++;
+
+        token = strtok(NULL, " \t");
+
+    }
+
+ 
 
     args[i] = NULL;
 
-    if(i = 0 && strcmp(args[i-1],"&") == 0){
+ 
+
+    if (i > 0 && strcmp(args[i - 1], "&") == 0) {
+
         background = 1;
+
         args[i - 1] = NULL;
+
     }
 
+ 
+
     return background;
+
 }
 
+ 
+
+void execute_command(char *args[], int background) {
+    pid_t pid;
+    pid = fork();
+    if (pid < 0) {
+        printf("Erro ao criar processo-filho\n");
+    }
+
+    else if (pid == 0) {
+        execvp(args[0], args);
+        perror("Erro ao executar comando");
+        exit(1);
+    }
+    else {
+        if (!background) {
+            wait(NULL);
+        }
+    }
+}
 void execute_command(char *args[], int background){
     pid_t pid;
     pid = fork();
